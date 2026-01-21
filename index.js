317a43dc90e75ca765a7ddba33231309668dc964
Atom Enoch <atomenoch45@gmail.com>
2026-01-21 11:54:55 -0500

    Syntax error preventing render

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(express.json());
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin) return callback(null, true);
-        if (allowedOrigins.includes((origin){
+        if (allowedOrigins.includes(origin)){
             return callback(null, true);
-        )
+        }
         return callback(new Error("Not allowed by CORS"));
         }
     }));
